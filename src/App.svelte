e6191ae809e323c6fafc49944392a77176fb94a9
Ørjan Svendsen <orjan@knusk.eu>
2022-01-12 19:48:13 +0100

    Add recipes and recipe tile

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
+	import RecipeTile from './components/RecipeTile.svelte';
+	import recipes from "./recipes.js";
 	export let name;
 </script>
 
 <main>
 	<h1> Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#each recipes as r}
+		<RecipeTile title={r.title} ingredients={r.ingredients} />
+	{/each}
 </main>
 
 <style type="text/scss">

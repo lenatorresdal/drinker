c05ef368562b5beb6293ed7ea884c9cfbd0dd55d
lenatorresdal <letorresdal@gmail.com>
2022-01-12 19:55:29 +0100

    Added flex container

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,12 @@
 <script>
-	export let name;
+  import FlexContainer from "./FlexContainer.svelte";
 </script>
 
 <main>
-	<h1> Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Drinkoppskrifter</h1>
+  <FlexContainer />
 </main>
 
 <style type="text/scss">
-@import "./styles.scss";
-
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: $blue;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-
+  @import "./styles.scss";
 </style>
---
+++ src/FlexContainer.svelte
@@ -0,0 +1,9 @@
+<div class="container" />
+
+<style>
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
